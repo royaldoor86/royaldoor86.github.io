0d7fafc8e6a79cb5b4be94e011647ede904de203
royaldoor86 <royaldoor86@gmail.com>
2026-04-22 21:46:18 +0300

    Add particle.js configuration for visual effects

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 150, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": "#f3cf7a" }, // لون ذهبي مثل تصميمك
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.5, "random": true },
+    "size": { "value": 3, "random": true },
+    "line_linked": { "enable": true, "distance": 150, "color": "#f3cf7a", "opacity": 0.4, "width": 1 },
+    "move": { "enable": true, "speed": 2, "direction": "none", "random": true, "out_mode": "out" }
+  },
+  "interactivity": {
+    "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" } }
+  }
+});
